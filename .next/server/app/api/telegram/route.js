859201c934909f43595d51adcae48970e3b84f95
strad-dev131 <stradiogamer@gmail.com>
2025-07-18 01:05:35 +0530

    Add .next/server/app/api/telegram/route.js 1752780936586

---
+++ .next/server/app/api/telegram/route.js
@@ -0,0 +1,8 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/telegram/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_node-fetch_src_utils_multipart-parser_2ca413cc.js");
+runtime.loadChunk("server/chunks/node_modules_ce8c41b9._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__0a7cd547._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/telegram/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/telegram/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/telegram/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
